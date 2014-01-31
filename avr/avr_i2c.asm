4d420afc1750a13b637b5fe6ff50e8493679fa75
sqfink <sqfink@gmail.com>
2014-01-31 00:03:04 -0500

    Compiles

--- avr/avr_i2c.asm
+++ avr/avr_i2c.asm
@@ -6,7 +6,8 @@
 ;   Authors:    Samuel Fink and Ryan Fredette
 ;----------------------------------------------------------;
 
-.DEVICE ATTINY84                            ;Run on the ATTINY 84
+.DEVICE  ATtiny84                            ;Run on the ATTINY 84
+.INCLUDE "tn84def.inc"
 
 ;-------------------DEFINE CONSTANTS-----------------------;
 .EQU    SLAVE_ADDR     =0x43                ;define the slave device address. MAXIMUM 7 BITS
@@ -19,115 +20,147 @@
 .EQU    S_OUT_MASK     =0b00000111          ;define bitmask for the pins that are used by the shift register
 .EQU    STATUS_START   =1                   ;set the enum for the start state of the i2c processing code
 .EQU    STATUS_US      =2                   ;set the enum for the reading state of the i2c processing code
-.EQU    STATUS_NOTUS   =3                   ;set the enum for the data ignore state of the i2c processing code
+.EQU    STATUS_NOT_US  =3                   ;set the enum for the data ignore state of the i2c processing code
 .EQU    STATUS_DONE    =4                   ;set the enum for we cant store any more data
 .EQU    USI_COUNT_MASK =0b00001111          ;define bitmaks for the USI 4 bit counter
 .EQU    USIOFI_MASK    =0b01000000          ;define the bitmask for the USI overflow interupt enable flag   
 
 ;-----------------SET REGISTER NAMES-----------------------;
-.DEF    BYTE_BUF       =R0                  ;store the current rendering byte in R0
-.DEF    BYTE_POS       =R1                  ;store the current offset of the rendering in R1
-.DEF    READ_POS       =R2                  ;store the current byte read offset in position in R2
-.DEF    I2C_STATUS     =R3                  ;store the current i2c status in R3
-.DEF    TEMP           =R4                  ;temp register
+.DEF    BYTE_BUF       =R17                  ;store the current rendering byte in R0
+.DEF    BYTE_POS       =R18                  ;store the current offset of the rendering in R1
+.DEF    READ_POS       =R19                  ;store the current byte read offset in position in R2
+.DEF    I2C_STATUS     =R20                  ;store the current i2c status in R3
+.DEF    TEMP           =R21                  ;temp register
+.DEF    PORTA_VAL      =R22                  ;what the PORTA value uhould be
+
+.CSEG
+;--------------------SET INTERRUPTS------------------------;
+.ORG    $0000
+            rjmp        setup               ;specify entry point
+.ORG    OVF1addr
+            rjmp        update              ;set update to handle timer 0 overflows
+.ORG    USI_OVFaddr
+            rjmp        i2c_byte_ready      ;set i2c_byte_ready to handle overflow of the bits recieved counter of the USI
+.ORG    USI_STRaddr
+            rjmp        i2c_start           ;set i2c_start to handle USI start condition
 
 ;---------------------SET UP DATA--------------------------;
 .DSEG                                       ;start data segment
     bytebuff: .BYTE CHANNEL_COUNT           ;reserve CHANNEL_COUNT bytes for storing PWM values 
 
-;--------------------SET INTERRUPTS------------------------;
-.ORG    $0000
-            rjmp        setup               ;specify entry point
-.ORG    TIM0_OVF
-            rjmp        update              ;set update to handle timer 0 overflows
-.ORG    USI_OVF
-            rjmp        i2c_byte_ready      ;set i2c_byte_ready to handle overflow of the bits recieved counter of the USI
-.ORG    USI_STR
-            rjmp        i2c_start           ;set i2c_start to handle USI start condition
-
 ;-----------------START CODE SEGMENT-----------------------;
 .CSEG                                       ;begin code segment
+.ORG 0x60
 
 setup:                                      ;system entry point
-    ser     DDRA,       0b10101111          ;set the pin directions for port A 
-    ser     USICR,      0b10101000          ;USI control enable start interrupt, 2 wire mode, clocked on external pos edge
-	ser     OCR0A,      0xFF                ;Set Timer 0 to count to max value
-	ser     TCCR0A,     2                   ;Set waveform mode to CTC mode
-	ser     TCCR0B,     4                   ;Set timer 0 prescaler to 256
-	ser     TIMSCK0,    1                   ;Enable Timer 0 overflow interrupt
-	ser     TCCR1B,     0b00010100          ;Set clock prescale to 256 and WGM to CTC mode
+	ldi		TEMP,       $AF
+    out     DDRA,       TEMP                ;set the pin directions for port A   (0b10101111)
+    ldi     TEMP,       $A8 
+	out     USICR,      TEMP                ;USI control enable start interrupt, 2 wire mode, clocked on external pos edge  (0b10101000)
+	ldi     TEMP,       $FF
+	out     OCR0A,      TEMP                ;Set Timer 0 to count to max value
+	ldi     TEMP,       $2
+	out     TCCR0A,     TEMP                ;Set waveform mode to CTC mode
+	ldi     TEMP,       $4
+	out     TCCR0B,     TEMP                ;Set timer 0 prescaler to 256
+	ldi     TEMP,       $1
+	out     TIMSK0,     TEMP                ;Enable Timer 0 overflow interrupt
+	ldi     TEMP,       $14
+	out     TCCR1B,     TEMP                ;Set clock prescale to 256 and WGM to CTC mode  (0b00010100)
     sei                                     ;set the global interrupt enable 
 main:                                       ;main wait loop 
     nop                                     ;do nothing TODO: replace this with sleep mode
     rjmp    main                            ;go back to main loop
 
 update:                                     ;callback for overflow of TCNT0
-    ld      YH,         high(bytebuff)      ;load the high byte of the address into the pointer
-    ld      YL,         low(bytebuff)       ;load the low byte of the address into the pointer
-    adiw    Y,          CHANNEL_COUNT       ;offset the read location start
+    ldi      YH,         high(bytebuff)     ;load the high byte of the address into the pointer
+    ldi      YL,         low(bytebuff)      ;load the low byte of the address into the pointer
+    adiw     Y,          CHANNEL_COUNT      ;offset the read location start
 loop:                                       ;begin pwm byte processing loop 
-    ld      BYTE_BUF,   -Y                  ;decrement the pointer and load the current byte of color data from sram
-    cbr     PORTA,      S_OUT_MASK          ;Clear the DS, SHCP, and STCP bits from the output
-    cp      TCNT1H,     BYTE_BUF            ;compare the current byte value and the Timer1 High byte value
-    brh     nosethigh                       ;skip setting the data pin if Timer1H > current byte
-    sbr     PORTA,      DS_PIN              ;else set DS high
+    ld      BYTE_BUF,    -Y                 ;decrement the pointer and load the current byte of color data from sram
+    cbr     PORTA_VAL,   S_OUT_MASK         ;Clear the DS, SHCP, and STCP bits from the output
+	out     PORTA,       PORTA_VAL          ;
+	in      TEMP,        TCNT1H
+    cp      BYTE_BUF,    TEMP               ;compare the current byte value and the Timer1 High byte value
+    brlo    nosethigh                       ;skip setting the data pin if Timer1H > current byte
+    sbr     PORTA_VAL,   DS_PIN             ;else set DS high
+	out     PORTA,       PORTA_VAL
 nosethigh:
-    sbr     PORTA,      SHCP_PIN            ;set SHCP(clock) high
+    sbr     PORTA_VAL,   SHCP_PIN           ;set SHCP(clock) high
+	out     PORTA,       PORTA_VAL          ;
     dec     BYTE_POS                        ;set next byte pointer
     tst     BYTE_POS                        ;compare the postions of the start of the data and the current byte position
-    brne    loop,                           ;if there are still bytes in the array jump to handle next byte
+    brne    loop                            ;if there are still bytes in the array jump to handle next byte
 finalize:
-    sbr     PORTA,      STCP_PIN            ;set STCP(clock) high
-    cbr     PORTA,      S_OUT_MASK          ;Clear the DS, SHCP, and STCP bits from the output
+    sbr     PORTA_VAL,  STCP_PIN            ;set STCP(clock) high
+	out     PORTA,      PORTA_VAL           ;
+    cbr     PORTA_VAL,  S_OUT_MASK          ;Clear the DS, SHCP, and STCP bits from the output
+	out     PORTA,      PORTA_VAL          ;
     reti                                    ;return from interrupt     
 
 i2c_start:                                  ;callback for the i2c start interrupt
-    set     I2C_STATUS, STATUS_START        ;set the status register to started
-    cbr     USISR,      USI_COUNT_MASK      ;clears the USI bitcounter      
-    sbr     USICR,      USIOFI_MASK         ;set the USI bit counter overflow interrupt enable
+    ldi     I2C_STATUS, STATUS_START        ;set the status register to started
+	in      TEMP,       USISR
+    cbr     TEMP,       USI_COUNT_MASK      ;clears the USI bitcounter   
+	out     USISR,      TEMP
+	in      TEMP,       USICR   
+    sbr     TEMP,       USIOFI_MASK         ;set the USI bit counter overflow interrupt enable
+	out     USICR,      TEMP
     reti                                    ;interrupt return
 
 i2c_byte_ready:                             ;callback for the USI byte ready interrupt (bit count overflow)
     cli                                     ;disable global interrupts
     cpi     I2C_STATUS, STATUS_START        ;check if this is a new I2C packet
     brne    skip_check_addr                 ;was not an address packet skip checking the addr
-    mov     TEMP,       USIDR               ;copy the data from the I2C register to the temp storage
+    in      TEMP,       USIDR               ;copy the data from the I2C register to the temp storage
     lsr     TEMP                            ;right shift the address packet to remove the R/W bit
     cpi     TEMP,       SLAVE_ADDR          ;check address against out address
     breq    i2c_was_us                      ;if it the same handle it
-    mov     I2C_STATUS, STATUS_NOT_US       ;set the flag that we are not handling this packet
-    cbr     USICR,      USIOFI_MASK         ;dissable the USI counter overflow interrupt so further bytes are ignored
+    ldi     I2C_STATUS, STATUS_NOT_US       ;set the flag that we are not handling this packet
+	in      TEMP,      USICR
+    cbr     TEMP,      USIOFI_MASK         ;dissable the USI counter overflow interrupt so further bytes are ignored
+	out     USICR,     TEMP
     rjmp    i2c_finish_byte                 ;done handling the byte GOTO exit byte callback
 i2c_was_us:
-    mov     I2C_STATUS, STATUS_US           ;set the status flag to was us  
-    cbr     USISR,      USI_COUNT_MASK      ;clear the USI counter
-    set     READ_POS,   CHANNEL_COUNT       ;reset the current read offset to channel count (1 past end of array)
-    set     ZH,         high(bytebuff)      ;load the high byte of the address into the pointer
-    set     ZL,         low(bytebuff)       ;load the low byte of the address into the pointer
-    adiw    ZH,         CHANNEL_COUNT       ;offset the read location start
-    sbr     USICR,      USIOFI_MASK         ;enable counter overflow interrupt
+    ldi     I2C_STATUS, STATUS_US           ;set the status flag to was us  
+	in      TEMP,       USISR
+    cbr     TEMP,       USI_COUNT_MASK      ;clear the USI counter
+	out     USISR,      TEMP
+    ldi     READ_POS,   CHANNEL_COUNT       ;reset the current read offset to channel count (1 past end of array)
+    ldi     ZH,         high(bytebuff)      ;load the high byte of the address into the pointer
+    ldi     ZL,         low(bytebuff)       ;load the low byte of the address into the pointer
+    adiw    Z,          CHANNEL_COUNT       ;offset the read location start
+	in      TEMP,       USICR
+    sbr     TEMP,       USIOFI_MASK         ;enable counter overflow interrupt
+	out     USICR,      TEMP
     rjmp    i2c_ack                         ;send ack
 skip_check_addr:
     cpi     I2C_STATUS, STATUS_US           ;check if we are reading bytes in this packet
     brne    i2c_finish_byte                 ;we are not reading this packed, note: this jump should never occur
-    st      -Z,         USIDR               ;decrement Z and copy the data from the USI data register to the adddress in Z
+	in      TEMP,       USIDR
+    st      -Z,         TEMP                ;decrement Z and copy the data from the USI data register to the adddress in Z
     dec     READ_POS                        ;decrement the next byte read location  
     tst     READ_POS                        ;check if out of bytes we can store
     brne    i2c_ack                         ;we can read more so move to ack
-    mov     I2C_STATUS, STATUS_DONE         ;we cant read any more so set status to out of space        
+    ldi     I2C_STATUS, STATUS_DONE         ;we cant read any more so set status to out of space        
 i2c_ack:
-    mov     TEMP,       PINA                ;copy the data direction register into temp
+    in      TEMP,       PINA                ;copy the data direction register into temp
     sbr     TEMP,       SCL_PIN             ;clear the clock pin from the temp
-    cp      TEMP,       PINA                ;check if the SCL pin was set
+    in      TEMP,       PINA                ;check if the SCL pin was set
     breq    ack_SCL_high                    ;The SCL was high
 ack_SCL_low:
-    sbr     DDRA,       SDA_PIN             ;bring SDA high for ack signal  
+    in      TEMP,       DDRA
+    sbr     TEMP,       SDA_PIN             ;bring SDA high for ack signal
+	out     DDRA,       TEMP  
     rjmp    i2c_ack                         ;loop until the clock goes high
 ack_SCL_high:
     nop                                     ;slight delay for propogation of pin value on bus
-    cbr     DDRA,       SDA_PIN             ;bring SDA low for ending ack signal                
+	in      TEMP,       DDRA
+    cbr     TEMP,       SDA_PIN             ;bring SDA low for ending ack signal      
+	out     DDRA,       TEMP          
 i2c_finish_byte:
-    clr     USIDR                           ;clear the USI data register
+    clr     TEMP                            
+	out     USIDR,      TEMP                ;clear the USI data register
     sei                                     ;re-enable global interrupts
     reti                                    ;interrupt return
 
