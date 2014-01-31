0882d0f33287103b6b10a725815d1167536d4df3
Ryan Fredette <rfredette27@gmail.com>
2014-01-30 21:45:26 -0500

    Changing some commands from pseudocode to real AVR commands

--- avr/avr_i2c.asm
+++ avr/avr_i2c.asm
@@ -52,19 +52,19 @@ main:                                       ;main wait loop
     rjmp    main                            ;go back to main loop
 
 update:                                     ;callback for overflow of TCNT0
-    ser     YH,         high(bytebuff)      ;load the high byte of the address into the pointer
-    ser     YL,         low(bytebuff)       ;load the low byte of the address into the pointer
+    ld      YH,         high(bytebuff)      ;load the high byte of the address into the pointer
+    ld      YL,         low(bytebuff)       ;load the low byte of the address into the pointer
     adiw    Y,          CHANNEL_COUNT       ;offset the read location start
 loop:                                       ;begin pwm byte processing loop 
     ld      BYTE_BUF,   -Y                  ;decrement the pointer and load the current byte of color data from sram
     cbr     PORTA,      S_OUT_MASK          ;Clear the DS, SHCP, and STCP bits from the output
-    cmp     TCNT1H,     BYTE_BUF            ;compare the current byte value and the Timer1 High byte value
+    cp      TCNT1H,     BYTE_BUF            ;compare the current byte value and the Timer1 High byte value
     brh     nosethigh                       ;skip setting the data pin if Timer1H > current byte
     sbr     PORTA,      DS_PIN              ;else set DS high
 nosethigh:
     sbr     PORTA,      SHCP_PIN            ;set SHCP(clock) high
     dec     BYTE_POS                        ;set next byte pointer
-    cmp     BYTE_POS,   0                   ;compare the postions of the start of the data and the current byte position
+    tst     BYTE_POS                        ;compare the postions of the start of the data and the current byte position
     brne    loop,                           ;if there are still bytes in the array jump to handle next byte
 finalize:
     sbr     PORTA,      STCP_PIN            ;set STCP(clock) high
@@ -79,15 +79,15 @@ i2c_start:                                  ;callback for the i2c start interrup
 
 i2c_byte_ready:                             ;callback for the USI byte ready interrupt (bit count overflow)
     cei                                     ;disable global interrupts
-    cmp     I2C_STATUS, STATUS_START        ;check if this is a new I2C packet
+    cpi     I2C_STATUS, STATUS_START        ;check if this is a new I2C packet
     brne    skip_check_addr                 ;was not an address packet skip checking the addr
     mov     TEMP,       USIDR               ;copy the data from the I2C register to the temp storage
     lsr     TEMP                            ;right shift the address packet to remove the R/W bit
-    cmp     TEMP,       SLAVE_ADDR          ;check address against out address
+    cpi     TEMP,       SLAVE_ADDR          ;check address against out address
     breq    i2c_was_us                      ;if it the same handle it
     mov     I2C_STATUS, STATUS_NOT_US       ;set the flag that we are not handling this packet
     cbr     USICR,      USIOFI_MASK         ;dissable the USI counter overflow interrupt so further bytes are ignored
-    ijmp    i2c_finish_byte                 ;done handling the byte GOTO exit byte callback
+    rjmp    i2c_finish_byte                 ;done handling the byte GOTO exit byte callback
 i2c_was_us:
     mov     I2C_STATUS, STATUS_US           ;set the status flag to was us  
     crb     USISR,      USI_COUNT_MASK      ;clear the USI counter
@@ -96,23 +96,23 @@ i2c_was_us:
     set     ZL,         low(bytebuff)       ;load the low byte of the address into the pointer
     adiw    ZH,         CHANNEL_COUNT       ;offset the read location start
     sbr     USICR,      USIOFI_MASK         ;enable counter overflow interrupt
-    ijmp    i2c_ack                         ;send ack
+    rjmp    i2c_ack                         ;send ack
 skip_check_addr:
-    cmp     I2C_STATUS, STATUS_US           ;check if we are reading bytes in this packet
+    cpi     I2C_STATUS, STATUS_US           ;check if we are reading bytes in this packet
     brne    i2c_finish_byte                 ;we are not reading this packed, note: this jump should never occur
     st      -Z,         USIDR               ;decrement Z and copy the data from the USI data register to the adddress in Z
     dec     READ_POS                        ;decrement the next byte read location  
-    cpi     READ_POS,   0                   ;check if out of bytes we can store
+    tst     READ_POS                        ;check if out of bytes we can store
     brne    i2c_ack                         ;we can read more so move to ack
     mov     I2C_STATUS, STATUS_DONE         ;we cant read any more so set status to out of space        
 i2c_ack:
     mov     TEMP,       PINA                ;copy the data direction register into temp
     sbr     TEMP,       SCL_PIN             ;clear the clock pin from the temp
-    cmp     TEMP,       PINA                ;check if the SCL pin was set
+    cp      TEMP,       PINA                ;check if the SCL pin was set
     breq    ack_SCL_high                    ;The SCL was high
 ack_SCL_low:
     sbr     DDRA,       SDA_PIN             ;bring SDA high for ack signal  
-    ijmp    i2c_ack                         ;loop until the clock goes high
+    rjmp    i2c_ack                         ;loop until the clock goes high
 ack_SCL_high:
     nop                                     ;slight delay for propogation of pin value on bus
     cbr     DDRA,       SDA_PIN             ;bring SDA low for ending ack signal                
